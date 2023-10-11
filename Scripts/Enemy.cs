0d71dd175e2805517ff71a9d6e1510214dc4f519
Matt Ware <mattbware3@gmail.com>
2023-10-11 05:50:42 +0100

    Misc setup

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -2,16 +2,19 @@ using Godot;
 
 public partial class Enemy : Node2D
 {
-	[Export] public Vector2 SpeedRange = new(150, 250);
+	[Export] public Vector2 speedRange = new(150, 250);
+	[Export] public Vector2 hpRange = new(150, 250);
 
 	private float _speed;
+	private float _hp;
 	private Vector2 _dir = Vector2.Right;
 	
 	public override void _Ready()
 	{
 		base._Ready();
 		var rng = new RandomNumberGenerator();
-		_speed = rng.RandfRange(SpeedRange.X, SpeedRange.Y);
+		_speed = rng.RandfRange(speedRange.X, speedRange.Y);
+		_hp = rng.RandfRange(hpRange.X, hpRange.Y);
 	}
 
 	public override void _Process(double delta)
